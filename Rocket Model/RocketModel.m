d95a3d08b0069d16ebef89b3d3a26378bf0d94ef
ncrnkovich <62952982+ncrnkovich@users.noreply.github.com>
2020-11-29 22:37:18 -0600

    Added airspeed calculations

--- Rocket Model/RocketModel.m
+++ Rocket Model/RocketModel.m
@@ -8,48 +8,84 @@
 % data to be used for controller and ACS design
 
 
-% Initialize rocket conditions
-Ground = [1; 1; 1];
+%% Initialize rocket conditions
+u = 10; % [mph] windspeed
+theta = 5; % [deg] launch angle
+dt = 0.01;
+
+weightR = 100; % [lbf] 
+lengthR = 134/12; % [ft] length of rocket
+diamR = 6.17/12; % [ft] outer diameter of rocket
 
 
-w = 10; % windspeed (pick units)
-alpha = 5; % [deg] launch angle
-v0 = 0; % initial velocity
-RocketPos = 0.*Ground; % initial position
+% World Coord i, j, k
+% i is into the wind and direction the launch rail is canted
+% k is vertical
+ihat = [1; 0; 0]; j = [0; 1; 0]; k = [0; 0; 1];
+
+% Rocket coord Ri, Rj, Rk
+% +Ri points from nose cone down rocket body
+% +Rj aligns with i-k plane
+Ri = -sind(theta)*ihat - cosd(theta)*k;
+Rj = cosd(theta)*ihat - sind(theta)*k;
+Rk = -j;
+
+% Initial conditions
+RocketVel = 0*Ri + 0*Rj + 0*Rk; % initial velocity
+RocketPos = [lengthR*sind(theta); 0; lengthR*cosd(theta)];
 
 % Compute local wind velocity and atmospheric conditions
 rho_air = 0.002378; % [slugs/ft^3] density of air
 c_air = 1120; % [ft/s] speed of sound at 70 deg F
 T = 68; % [def F] atmospheric temperature
 
+[windTime, windspeed] = windGenerator(u); % windspeed is in mph
+wfps = windspeed.*5280/3600; % convert to [ft/s]
 
+rocketSpeed = abs(RocketVel);
 
-
-
-% Compute airspeed, angle of attack, lateral wind speed, Reynolds number...
-
-
-
-
-% Compute aerodynamic forces and moments
-
-% Base drag
-
-% body pressure drag
-
-% Parasitic drag from tabs
-
-
-
-
-% Compute effect of motor thrust and gravity
-
-% Mass and force from motor
-[motorThrust, motorMass] = motorfunction(t); % returns mass in oz and thrust in lbf
-
-
-% Compute mass, moments of inertia, and linear and angular acceleration
-
-
-
-% Numerical integration using RK 4
+%% Run loop
+while rocketSpeed > -2
+    t = 0;
+    iCnt = 1;
+    
+    
+    % Compute airspeed, angle of attack, lateral wind speed, Reynolds number...
+    w_wind = (wfps(iCnt) + (t - windTime(iCnt))*(wfps(iCnt+1) - wfps(iCnt))...
+        /(windTime(iCnt+1) - windTime(iCnt)))*-ihat; % Interpolate windspeed
+    while rocketPos(3) < 2 % Wind is negligible til it leaves the rail
+        w_wind = 0;
+    end
+    
+    airspeed = RocketVel - w_wind; 
+    
+    
+    % Compute aerodynamic forces and moments
+    
+    % Base drag
+    
+    % body pressure drag
+    
+    % Parasitic drag from tabs
+    
+    
+    
+    
+    % Compute effect of motor thrust and gravity
+    
+    % Mass and force from motor
+    [motorThrust, motorMass] = motorfunction(t); % returns mass in oz and thrust in lbf
+    
+    
+    % Compute mass, moments of inertia, and linear and angular acceleration
+    
+    
+    
+    % Numerical integration using RK 4
+    
+    
+    
+    RocketSpeed = -5;
+    t = t + dt;
+    iCnt = iCnt + 1;
+end
