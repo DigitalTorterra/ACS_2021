04f9bb3b8d560c90d4bd852d9408846e5505e9cc
Savannah Bedford <savannahbedford@sbedford01-mac.dhcp.nd.edu>
2021-02-09 21:21:29 -0500

    my additons :)))

--- state.py
+++ state.py
@@ -30,7 +30,45 @@ def state_transition(data):
     vertical position, velocity, and acceleration
     Output: None
     """
+    #want: current state num, current state str,
+    #nextstate = le_current_state
+    # want: series of if statements for each state_values
     height, velocity, acceleration, *_ = data
+
+    if RNstate == 0: # state_values is armed
+        rock.LED.on()
+        if acceleration > liftoff acceleration or height > liftoff height:
+            new_state = 1
+
+    if RNstate == 1 # state_values is launched
+        if acceleration < liftoff acceleration or height > burnout height:
+            new_state = 1
+            
+    if RNstate == 2 # state_values is burnout
+        if acceleration > burnout acceleration:
+            new_state = 1 #launch sate_values bc accel noise
+        if velocity < 0:
+            new_state = 3 #state_values is apogee
+            # apogee = best scrabble word ever
+        if height > apogee and velocity > 0:
+            new_state = 4 #state_values is overshot
+
+        if RNstate == 3: # state_values is apogee :))
+            rock.LED.on()
+            if velocity > 0:
+                new_state = 2
+            if velocity <= 10 and height <=10 and acceleration <=0:
+                new_state = 5 #state_values is landed
+
+        if RNstate == 4: #state_values is overshoot
+            if velocity <= 0:
+                new_state = 3 #state_values is apogee
+
+        RNstate = new_state
+        rock.LED.off()
+                
+            
+            
 
 def get_state_num():
     """
@@ -39,7 +77,8 @@ def get_state_num():
     Input: None
     Output: number of current state
     """
-    return 0
+    return state
+    #return 0
 
 def get_state_name():
     """
@@ -49,4 +88,4 @@ def get_state_name():
     Input: None
     Output: string containing the current state
     """
-    return "Armed"
+    return state_values[state]
